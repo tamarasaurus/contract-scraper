771c4e7e8b0b22ad732c42e1af2c540669a6af1c
Tamara Robichet <tamara.robichet@akeneo.com>
2019-04-23 15:34:45 +0200

    Add test for text type

---
+++ test/unit/attribute/text.test.ts
@@ -0,0 +1,12 @@
+import * as assert from 'assert';
+import Text from '../../../src/attribute/text';
+
+describe('creates a size attribute', () => {
+    it('extracts a trimmed text from a string', () => {
+        assert.equal(new Text(' Some text with some spaces ').getValue(), 'Some text with some spaces');
+    });
+
+    it('returns null for an empty string', () => {
+        assert.equal(new Text('').getValue(), null);
+    })
+});
